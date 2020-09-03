cd0c6765711faa5590d80f3bc1fc581ef7118eae
jerryjcbai <1018679568@qq.com>
2020-09-03 17:12:09 +0800

    add TPS flying mode

---
+++ Assets/Scripts/AutoNavi.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AutoNavi : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    private Vector3 _destination;
+    private void ToDestination(Vector3 dest)
+    {
+        _destination=dest;
+        _navMeshAgent.SetDestination(dest);
+    }
+    void Awake()
+    {
+        _navMeshAgent=GetComponent<NavMeshAgent>();
+        _animator=GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                if(hit.collider.name=="Plane")
+                {
+                    _destination=hit.point;
+                    _navMeshAgent.SetDestination(_destination);
+                }
+            }
+        }
+    }
+    void FixedUpdate() {
+        if(_navMeshAgent.velocity.magnitude>0.01f)
+        {
+            _animator.SetFloat("Speed",0.2f);
+        }
+        if(_navMeshAgent.velocity.magnitude<0.1f||Vector3.Distance(_destination,transform.position)<0.4f)
+        {
+            _navMeshAgent.velocity=Vector3.zero;
+            _animator.SetFloat("Speed",0);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Animator _animator;
+    public float _speed=1;
+    void Start()
+    {
+        _animator=GetComponent<Animator>();
+    }
+    private void Move(Vector3 destPos)
+    {
+        StopAllCoroutines();
+        StartCoroutine(IMove(destPos));
+    }
+    private IEnumerator IMove(Vector3 destPos)
+    {
+        while(Vector3.Distance(destPos,transform.position)>0.001)
+        {
+            Vector3 dir=Vector3.Normalize(destPos-transform.position);
+            transform.Translate(dir*_speed*Time.deltaTime);
+            yield return null;
+        }
+        transform.position=destPos;
+        //yield break;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                if(hit.collider.name=="Plane")
+                {
+                    Vector3 pos=hit.point;
+                    pos+=Vector3.up*0.5f;
+                    Move(pos);
+                }
+            }
+        }
+        // float vertical=Input.GetAxis("Vertical");
+        // float horizontal=Input.GetAxis("Horizontal");
+        // Vector3 dir=new Vector3(horizontal,0,vertical);
+        // if(dir!=Vector3.zero)
+        // {
+        //     _animator.SetBool("Run",true);
+        //     transform.Translate(dir*_speed*Time.deltaTime);
+        // }
+        // else
+        // {
+        //     _animator.SetBool("Run",false);
+        // }
+    }
+}
